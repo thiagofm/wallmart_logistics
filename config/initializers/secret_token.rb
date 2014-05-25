7fb8fbd2f437e27c97b14e544ecf0a2cbd3894fa
Thiago Fernandes Massa <thiagown@gmail.com>
2014-05-25 13:33:31 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WallmartLogistics::Application.config.secret_token = '1259eaf8f5f551edd6863e2021d950519f14c29e0bbf87748745d8f84b74aefeac1f332ed6ebaa8a2fd18f17d270f539bdc78e6ba5a69001fa43f90e2d4a2273'
